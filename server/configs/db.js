ad387dac24dc9c1678a380158f17c36fa430e5fc
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-10-23 07:36:40 +0530

    created userController, user & created db connection

---
+++ server/configs/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('MongoDB connected successfully')});
+
+            let mongodbURI = process.env.MONGODB_URI;
+            const projectName = 'resume-builder';
+
+            if(!mongodbURI) {
+                throw new Error('MONGODB_URI is not defined in environment variables');
+            }            
+
+            if(mongodbURI.endsWith('/')) {
+                mongodbURI = mongodbURI.slice(0, -1);
+            }
+            
+            await mongoose.connect(`${mongodbURI}/${projectName}`)
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+export default connectDB;
